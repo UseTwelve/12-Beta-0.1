b1a3c7f0073563573027259b216ad44cfde86111
eddie leftie <59047760+uwambajeddy@users.noreply.github.com>
2024-08-06 22:21:50 +0200

    fix reload

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 "use client";
 import { useSession } from 'next-auth/react';
 import { redirect } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function Home() {
   const { data: session, status } = useSession();
+
+  useEffect(() => { }, [session,status]);
 
   if (status === 'loading') return <p>Loading...</p>;
 
