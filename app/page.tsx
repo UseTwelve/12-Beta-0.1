c92a813876ce8c744634a9f8d9cc6ad5d4e59a31
eddie leftie <59047760+uwambajeddy@users.noreply.github.com>
2024-08-07 12:46:50 +0200

    fix reload last

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,26 @@
 "use client";
 import { useSession } from 'next-auth/react';
-import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
+import { redirect,useRouter } from 'next/navigation';
+import { use, useEffect } from 'react';
 
 export default function Home() {
   const { data: session, status } = useSession();
   const router = useRouter();
-
   useEffect(() => {
-    if (status === 'authenticated') {
-      if (session.user.userType.name === "admin") {
-        router.push('/admin/churches');
-      } else if (session.user.userType.name === "client") {
-        router.push('/client/giving');
-      }
-    } else if (status !== 'loading') {
-      router.push('/signin');
-    }
-  }, [session, status, router]);
+
+    if(session)  router.refresh();
+   }, [session,status]);
 
   if (status === 'loading') return <p>Loading...</p>;
 
-  return null; // Return null since the redirects handle all cases
+  if (status === 'authenticated') {
+    if (session.user.userType.name === "admin") {
+      return redirect('/admin/churches');
+    }
+    if (session.user.userType.name === "client") {
+      return redirect('/client/giving');
+    }
+  }
+
+  return redirect('/signin');
 }
